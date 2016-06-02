974842c6c526bd4744bb29677db5e373cbce82bd
lev2016 <virtualinsanity05@gmail.com>
2016-06-02 23:31:18 +0300

    1.0
    1.0

---
+++ AmazonSearch/Amazon/AmazonQuery.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ServiceModel;
+using System.Configuration;
+using AmazonSearch.PAAPI;
+using AmazonSearch;
+
+namespace AmazonSearch
+{
+    public class AmazonQuery
+    {
+
+        public AmazonQuery()
+        {
+
+        }
+
+        public ItemLookupResponse LookupByASIN(string[] ASINs, string[] Group)
+        {
+            BasicHttpBinding binding = new BasicHttpBinding(BasicHttpSecurityMode.Transport);
+            binding.MaxReceivedMessageSize = int.MaxValue;
+            binding.MaxBufferSize = int.MaxValue;
+
+            AWSECommerceServicePortTypeClient amazonClient = new AWSECommerceServicePortTypeClient(
+                        binding,
+                        new EndpointAddress("https://webservices.amazon.com/onca/soap?Service=AWSECommerceService"));
+            // add authentication to the ECS client
+            amazonClient.ChannelFactory.Endpoint.Behaviors.Add(new AmazonSigningEndpointBehavior(ConfigurationManager.AppSettings["accessKeyId"], ConfigurationManager.AppSettings["secretKey"]));
+
+            ItemLookupRequest request = new ItemLookupRequest();
+            request.ItemId = ASINs;
+            request.IdType = ItemLookupRequestIdType.ASIN;
+            request.ResponseGroup = Group;
+
+            ItemLookup itemLookup = new ItemLookup();
+            itemLookup.Request = new ItemLookupRequest[] { request };
+            itemLookup.AWSAccessKeyId = ConfigurationManager.AppSettings["accessKeyId"];
+            itemLookup.AssociateTag = ConfigurationManager.AppSettings["associateTag"];
+
+            ItemLookupResponse response = amazonClient.ItemLookup(itemLookup);
+            return response;
+        }
+
+
+        public ItemSearchResponse ItemSearch(string SearchIndex, string[] Group, string Keywords)
+        {
+            BasicHttpBinding binding = new BasicHttpBinding(BasicHttpSecurityMode.Transport);
+            binding.MaxReceivedMessageSize = int.MaxValue;
+            binding.MaxBufferSize = int.MaxValue;
+
+            AWSECommerceServicePortTypeClient amazonClient = new AWSECommerceServicePortTypeClient(
+                        binding,
+                        new EndpointAddress("https://webservices.amazon.com/onca/soap?Service=AWSECommerceService"));
+            // add authentication to the ECS client
+            amazonClient.ChannelFactory.Endpoint.Behaviors.Add(new AmazonSigningEndpointBehavior(ConfigurationManager.AppSettings["accessKeyId"], ConfigurationManager.AppSettings["secretKey"]));
+
+            ItemSearchRequest search = new ItemSearchRequest();
+            search.SearchIndex = SearchIndex;
+            search.ResponseGroup = Group;
+            search.Keywords = Keywords;
+
+
+            ItemSearch itemSearch = new ItemSearch();
+            itemSearch.Request = new ItemSearchRequest[] { search };
+            itemSearch.AWSAccessKeyId = ConfigurationManager.AppSettings["accessKeyId"];
+            itemSearch.AssociateTag = ConfigurationManager.AppSettings["associateTag"];
+
+            ItemSearchResponse response = amazonClient.ItemSearch(itemSearch);
+            return response;
+        }
+
+    }
+}
---
+++ AmazonSearch/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using AmazonSearch.Models;
+using System.Collections.Generic;
+
+namespace AmazonSearch.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            string Keywords = "Gravity falls Mabel figure";
+
+            AmazonQuery search = new AmazonQuery();
+
+            string SearchIndex = "All";
+            string[] SearchResponseGroup = new string[] { "Medium" };
+
+            var searchResponse = search.ItemSearch(SearchIndex, SearchResponseGroup, Keywords);
+
+            List<ItemModel> itemModels = new List<ItemModel>();
+
+            for(int i = 0; i < searchResponse.Items[0].Item.Count(); i++)
+            {
+                var newItem = new ItemModel();
+                newItem.Title = searchResponse.Items[0].Item[i].ItemAttributes.Title;
+                newItem.Manufacturer = searchResponse.Items[0].Item[i].ItemAttributes.Manufacturer;
+                if (searchResponse.Items[0].Item[i].ItemAttributes.ListPrice != null)
+                {
+                    newItem.Price = searchResponse.Items[0].Item[i].ItemAttributes.ListPrice.Amount;
+                }
+                else
+                {
+                    newItem.Price = "Unfortunately, there is no price availible";
+                }
+                itemModels.Add(newItem);
+            }
+
+            return View(itemModels);
+        }
+    }
+  
+}
